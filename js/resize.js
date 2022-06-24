0e60ebd5c2dfd9ab2e2176626f422b1a72b4f85a
hyeeeonc <hyeeeonc@gmail.com>
2022-06-25 02:53:46 +0900

    header-resp,video-load fin

--- js/resize.js
+++ js/resize.js
@@ -1,10 +1,13 @@
 const pc = document.querySelector(".pc");
 const mobile = document.querySelector(".mobile");
+const mainVideo = document.getElementById("main-video");
+const mainVideo2 = document.getElementById("main-video2");
 document.addEventListener("DOMContentLoaded", () => {
   if (window.innerWidth < 768) {
     pc.remove();
   } else {
     mobile.remove();
+    mainVideo2.setAttribute("src", mainVideo.dataset.src);
   }
 });
 
